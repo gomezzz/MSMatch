83fab6aa30cac2a4dfad97a4f556e854b6e5c3a7
GabrieleMeoni <gabrielemeoni@kalypso.localdomain>
2020-11-09 20:43:22 +0100

    Added automatization on the number of classes (use --num_classes -1 to exploit the default value for the dataset) Added runscripts Edited tb directory to save model and tb logs into the Fixmatch_arch... dir

---
+++ runscripts/hyperparameter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=4
+
+FIXMATCH_FOLDER="$HOME/project/SSLRS/"
+SAVE_LOCATION="/scratch/fixmatch_results" #Where tensorboard output will be written
+SAVE_NAME="hyperparameters"
+NET=efficientNet                              #Options are wideResNet,efficientNet
+DATASET=eurosat_rgb                          #Dataset to use
+NUM_LABELS=1000
+UNLABELED_RATIO=7
+BATCH_SIZE=16
+N_EPOCH=32                    #Set NUM_TRAIN_ITER = N_EPOCH * NUM_EVAL_ITER
+NUM_EVAL_ITER=1000            #Number of iterations 
+NUM_TRAIN_ITER=$(($N_EPOCH * $NUM_EVAL_ITER))
+SEED=0
+
+#create save location
+mkdir -p $SAVE_LOCATION
+
+#switch to fixmatch folder for execution
+cd $FIXMATCH_FOLDER
+
+for LR in 0.03 0.1 0.2; do
+    for WEIGHT_DECAY in 0.0001 0.0005 0.001; do
+        echo python train.py --weight_decay $WEIGHT_DECAY --world-size 1 --rank 0 --multiprocessing-distributed --lr $LR --batch_size $BATCH_SIZE --num_train_iter $NUM_TRAIN_ITER --num_eval_iter $NUM_EVAL_ITER --num_labels $NUM_LABELS --save_name $SAVE_NAME --save_dir $SAVE_LOCATION --dataset $DATASET --num_classes -1 --net $NET --seed $SEED --uratio $UNLABELED_RATIO
+		wait
+    done
+done
---
+++ runscripts/nr_of_labels.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=4
+
+FIXMATCH_FOLDER="$HOME/project/SSLRS/"
+SAVE_LOCATION="/scratch/fixmatch_results" #Where tensorboard output will be written
+SAVE_NAME="nr_of_labels"
+NET=efficientNet                              #Options are wideResNet,efficientNet
+DATASET=eurosat_rgb                          #Dataset to use
+SEED=0
+BATCH_SIZE=16
+UNLABELED_RATIO=7
+WEIGHT_DECAY=0.0005
+N_EPOCH=32                    #Set NUM_TRAIN_ITER = N_EPOCH * NUM_EVAL_ITER
+NUM_EVAL_ITER=1000            #Number of iterations 
+NUM_TRAIN_ITER=$(($N_EPOCH * $NUM_EVAL_ITER))
+
+#create save location
+mkdir -p $SAVE_LOCATION
+
+#switch to fixmatch folder for execution
+cd $FIXMATCH_FOLDER
+
+for NUM_LABELS in 10 20 40 100 200 400; do #Note: they are the total number of labels, not per class.
+    echo python train.py --world-size 1 --rank 0 --multiprocessing-distributed --num_train_iter $NUM_TRAIN_ITER --num_eval_iter $NUM_EVAL_ITER --save_name $SAVE_NAME --weight_decay $WEIGHT_DECAY --batch_size $BATCH_SIZE --num_labels $NUM_LABELS --save_dir $SAVE_LOCATION --dataset $DATASET --num_classes -1 --net $NET --seed $SEED --uratio $UNLABELED_RATIO
+    wait
+done
