- Added albumentations to env - Ignored .vscode - Added ms_augmentation flag to dataset - Removed normalization from eurosat_ms - Converted eurosat_ms to uint8 - Fixed Transform order in ssl_dataset transforms - Added ms_augmentation flag in SSL dataset - Fixed wrong dataset selection in SSLDataset - Model is still broken - Added augmentations for MS with albumentations and imgaug - Added cutout for np.array images - Added some test code to randaugment

datasets/augmentation/randaugment.py
@@ -6,9 +6,15 @@
 import random
 
 import PIL, PIL.ImageOps, PIL.ImageEnhance, PIL.ImageDraw
+import albumentations as A
 import numpy as np
 import torch
 from PIL import Image
+
+try:  # to test locally by running python randaugment.py
+    from .ms_augmentations import ms_augmentation_list
+except:
+    from ms_augmentations import ms_augmentation_list
 
 
 def AutoContrast(img, _):
@@ -114,8 +120,14 @@ def Cutout(img, v):  # [0, 60] => percentage: [0, 0.2] => change to [0, 0.5]
     if v <= 0.0:
         return img
 
-    v = v * img.size[0]
-    return CutoutAbs(img, v)
+    if isinstance(img, np.ndarray):
+        v = int(v * img.shape[0])
+        return A.Cutout(1, v, v, always_apply=True, fill_value=(128))(image=img)[
+            "image"
+        ]
+    else:
+        v = v * img.size[0]
+        return CutoutAbs(img, v)
 
 
 def CutoutAbs(img, v):  # [0, 60] => percentage: [0, 0.2]
@@ -160,10 +172,13 @@ def augment_list():
 
 
 class RandAugment:
-    def __init__(self, n, m):
+    def __init__(self, n, m, use_ms_augmentations=False):
         self.n = n
         self.m = m  # [0, 30] in fixmatch, deprecated.
-        self.augment_list = augment_list()
+        if use_ms_augmentations:
+            self.augment_list = ms_augmentation_list()
+        else:
+            self.augment_list = augment_list()
 
     def __call__(self, img):
         ops = random.choices(self.augment_list, k=self.n)
@@ -176,7 +191,12 @@ class RandAugment:
 
 
 if __name__ == "__main__":
-    randaug = RandAugment(3, 5)
+    randaug = RandAugment(3, 5, True)
+    test_img = np.zeros([32, 32, 13], dtype="uint8")
     print(randaug)
-    for item in randaug.augment_list:
-        print(item)
+
+    for op, min_val, max_val in randaug.augment_list:
+        val = min_val + float(max_val - min_val) * random.random()
+        print(op)
+        img = op(test_img, val)
+

datasets/dataset.py
@@ -25,6 +25,7 @@ class BasicDataset(Dataset):
         use_strong_transform=False,
         strong_transform=None,
         onehot=False,
+        use_ms_augmentations=False,
         *args,
         **kwargs
     ):
@@ -37,6 +38,7 @@ class BasicDataset(Dataset):
             use_strong_transform: If True, this dataset returns both weakly and strongly augmented images.
             strong_transform: list of transformation functions for strong augmentation
             onehot: If True, label is converted into onehot vector.
+            use_ms_augmentations: If True will use albumentations and imgaug for augmentations (required for multispectral)
         """
         super(BasicDataset, self).__init__()
         self.data = data
@@ -44,13 +46,16 @@ class BasicDataset(Dataset):
 
         self.num_classes = num_classes
         self.use_strong_transform = use_strong_transform
+        self.use_ms_augmentations = use_ms_augmentations
         self.onehot = onehot
 
         self.transform = transform
         if use_strong_transform:
             if strong_transform is None:
                 self.strong_transform = copy.deepcopy(transform)
-                self.strong_transform.transforms.insert(0, RandAugment(3, 5))
+                self.strong_transform.transforms.insert(
+                    0, RandAugment(3, 5, use_ms_augmentations=use_ms_augmentations)
+                )
         else:
             self.strong_transform = strong_transform
 
@@ -77,7 +82,7 @@ class BasicDataset(Dataset):
         if self.transform is None:
             return transforms.ToTensor()(img), target
         else:
-            if isinstance(img, np.ndarray):
+            if isinstance(img, np.ndarray) and not self.use_ms_augmentations:
                 img = Image.fromarray(img)
             img_w = self.transform(img)
             if not self.use_strong_transform:

datasets/eurosat_dataset.py
@@ -36,7 +36,7 @@ class EurosatDataset(torch.utils.data.Dataset):
     def _load_data(self):
         """Loads the data from the passed root directory. Splits in test/train based on seed. By default resized to 256,256
         """
-        images = np.zeros([self.N, self.size[0], self.size[1], 13], dtype="float32")
+        images = np.zeros([self.N, self.size[0], self.size[1], 13], dtype="uint8")
         labels = []
         filenames = []
 
@@ -52,13 +52,10 @@ class EurosatDataset(torch.utils.data.Dataset):
                 # a few images are a few pixels off, we will resize them
                 image = imageio.imread(sub_f)
                 if image.shape[0] != self.size[0] or image.shape[1] != self.size[1]:
-                    # If we want to implement it check that datatypes don't make problems
-                    raise (
-                        NotImplementedError(
-                            "Resizing is currently not supported for this dataset."
-                        )
+                    image = img_as_ubyte(
+                        resize(image, (self.size[0], self.size[1]), anti_aliasing=True)
                     )
-                images[i] = img_as_float32(image)
+                images[i] = img_as_ubyte(image)
                 i += 1
                 labels.append(item)
 
@@ -100,17 +97,6 @@ class EurosatDataset(torch.utils.data.Dataset):
             idx = idx.tolist()
 
         img = self.data[idx]
-
-        # normalize per image to zero mean unit variance
-        if len(img.shape) == 3:
-            img = img / np.std(img)
-            img = img - np.mean(img)
-        elif len(img.shape) == 4:
-            for i in range(len(img)):
-                cur_img = img[i]
-                cur_img = cur_img / np.std(cur_img)
-                cur_img = cur_img - np.mean(cur_img)
-                img[i] = cur_img
 
         if self.transform:
             img = self.transform(img)