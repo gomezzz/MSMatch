170394a06eeb1fe8cd21fa6e5bb0476584400c0e
GabrieleMeoni <gabriele.meoni@esa.int>
2021-07-19 15:34:48 +0200

    Updated confusion matrix generation.

--- utils.py
+++ utils.py
@@ -11,11 +11,12 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 
 def plot_cmatrix(pred,labels,encoding, figsize=(8, 5),dpi=150, font_scale=1.2, save_fig_name=None):
-    cm = confusion_matrix(labels,pred)
+    cm = confusion_matrix(labels,preds,normalize='true')
+    cm=np.floor(cm*10000)/100
     sn.set(font_scale=font_scale) # for label size
     plt.figure(figsize=figsize, dpi=dpi)
     df_cm=pd.DataFrame(cm, index=[k for k in encoding], columns=[k for k in encoding])
-    sn.heatmap(df_cm, annot=True, linewidths=.5,fmt='g') # font size
+    sn.heatmap(df_cm, annot=True, linewidths=.5) # font size
     if save_fig_name is not None:
         sn.set_theme()
         plt.tight_layout()
